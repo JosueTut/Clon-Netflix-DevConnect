1795dcd4d6aa95641656cf9d5c9176f529c75899
Augusto Bobadilla <bobadillaaugusto3@gmail.com>
2023-09-29 17:48:27 -0300

    Correción de errores(III)

--- netflix-login/login-script.js
+++ netflix-login/login-script.js
@@ -6,5 +6,5 @@ document.getElementById("login").addEventListener("submit", function (event) {
     const contraseña = document.getElementById("input-password").value;
 
     // Redirigir a la página de inicio con los datos en la URL
-    window.location.href = 'home/home.html?nombre=${nombre}&contraseña=${contraseña}';
+    window.location.href = `home/home.html?nombre=${nombre}&contraseña=${contraseña}`;
 });
