9a17ba328b18814f4e21df0c94ec67007fe9b45c
Augusto Bobadilla <bobadillaaugusto3@gmail.com>
2023-09-29 17:27:33 -0300

    Cambios mínimos en el js y login

--- netflix-login/login-script.js
+++ netflix-login/login-script.js
@@ -1,8 +1,10 @@
+
+//Mandando los datos de usuario y contraseña a home.html
 document.getElementById("login").addEventListener("submit", function (event) {
     event.preventDefault(); // Evitar el envío del formulario por defecto
     const nombre = document.getElementById("input-usuario").value;
     const contraseña = document.getElementById("input-password").value;
 
     // Redirigir a la página de inicio con los datos en la URL
-    window.location.href = `home/home.html?nombre=${nombre}&contraseña=${contraseña}`;
+    window.location.href = "home/home.html?nombre=${nombre}&contraseña=${contraseña}";
 });
